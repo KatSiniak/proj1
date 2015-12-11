724b5800a740b527bf214ac6bc1df429597ed0ba
Екатерина <321213>
2015-12-11 17:03:46 +0300

    с фильтром

---
+++ WebApplication1/Helper/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Helper
+{
+    public class Help
+    {
+        public static List<Genres> AllGenres {
+            get {
+                return new List<Genres>
+                {
+                    Genres.Action,
+                    Genres.Comedy,
+                    Genres.Drama,
+                    Genres.History,
+                };
+            }
+        }
+    }
+}
